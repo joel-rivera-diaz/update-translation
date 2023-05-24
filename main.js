405205b4dc0031c1a390fe8226def357059f7238
joel-rivera-diaz <joelmrd@gmail.com>
2023-05-24 00:20:08 -0400

    mod property

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const path = require('path');
 const translationsFile = '/home/joel/dev_blaccspot/my-bsm-tools-trans/translations.json';
 const folders = ["de", "es", "fr", "ja", "ko", "pt-BR", "zh-CN", "zh-TW"];
 
-function updateJsonKey(jsonFileToModify, dotNotationStr) {
+function updateJsonKey(jsonFileToModify, dotNotationString) {
   folders.forEach((folder) => {
 
     // get value to assign
@@ -16,13 +16,36 @@ function updateJsonKey(jsonFileToModify, dotNotationStr) {
     const filePath = path.join(folder, jsonFileToModify);
 
     if (fs.existsSync(filePath)) {
+      // yes! it exists!
       console.log('file exists');
 
-      let fileContent = JSON.parse(fs.readFileSync(filePath));
-      const keys = dotNotationStr.split('.');
-      keys.slice(0, -1).forEach((key) => fileContent = fileContent[key]);
-      fileContent[keys[keys.length - 1]] = value;
-      fs.writeFileSync(filePath, JSON.stringify(fileContent, null, 2));
+      // get file
+      let jsonObj = JSON.parse(fs.readFileSync(filePath));
+      console.log({
+        jsonObj
+      });
+
+      // TODO: fix all the following code
+      // Get the value that will be modified using the dot notation string
+      const valueToModify = dotNotationString.split('.').reduce((obj, key) => obj[key], jsonObj);
+      
+      // if the property exists...
+      // ...assign the new value to the specified property
+      if (valueToModify) {
+        dotNotationString.split('.').reduce((obj, key, i, arr) => {
+          if (i === arr.length - 1) {
+            obj[key] = value;
+          }
+          return obj[key];
+        }, jsonObj);
+      } else {
+        console.log();
+      }
+
+      // Write the modified JSON back to the file
+      fs.writeFileSync(filePath, JSON.stringify(jsonObj, null, 2));
+
+      console.log(`Property "${dotNotationString}" modified.`);
     } else {
       console.log('no file found');
     }
